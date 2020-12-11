bb52b5ae3938ded54bdf78c2e94b69762be42da5
Seanbon0611 <seand0611@gmail.com>
2020-12-10 23:20:01 -0800

    updated godotenv package

--- main.go
+++ main.go
@@ -4,21 +4,23 @@ import (
 	"github.com/gin-gonic/gin"
 	// "github.com/jinzhu/gorm"
 	// _ "github.com/jinzhu/gorm/dialects/postgres"
+	// "os"
+	// "github.com/joho/godotenv"
 )
 
 func main() {
 	r := gin.Default()
-	// db, err := gorm.Open("postgres", "host=localhost port=5431 user=postgres dbname=test_db")
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World!",
+		})
+	})
+	// db, err := gorm.Open("postgres", "host=localhost port=5431 user=postgres dbname=test_db sslmode=disable")
 	// if err != nil {
 	// 	fmt.Println(err)
 	// 	panic("Failed to connect to Database")
 	// }
 	// fmt.Println("Database Connected")
 	// defer db.Close()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello World!",
-		})
-	})
 	r.Run()
 }
